077e7491e3b534063e69383b0a73e18d0b471881
VaughnA <vaughn_arjavac@davincischools.org>
2017-12-18 23:24:28 -0800

    Update movies.js

--- movies.js
+++ movies.js
@@ -1,8 +1,46 @@
-function makeHover(e){xxx
-	e.classList.toggle("hover");
+function myFunction5(elem) {
+    var x = document.getElementById("commentary4");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
 }
 
-function makeNormal(e){
-	e.classList.toggle("hover");
-	
+function myFunction6(elem) {
+    var x = document.getElementById("commentary5");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
 }
+
+function playSound1(elem) {
+	document.getElementById("big").play();
+}
+
+function playSound2(elem) {
+	document.getElementById("hot").play();
+}
+   
+function playSound3(elem) {
+	document.getElementById("forafew").play();
+}
+   
+function playSound4(elem) {
+	document.getElementById("memento").play();
+}
+   
+function playSound5(elem) {
+	document.getElementById("pulp").play();
+}
+   
+function playSound6(elem) {
+	document.getElementById("spinal").play();
+}
+   
+      
+
+
+//https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_toggle_hide_show//
